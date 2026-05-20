0d9a4c5647107daa58351323a33a4693ecc882d8
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-20 05:52:21 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+//let score= 33// number dataype
+let score="33" //string
+console.log(typeof score);
+
+//if you want your string value as number , then use this ...
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// suppose score="abc" then you will get datatype as number but if you go and print the value inside "valueInNumber" you will get NaN(Not a Number) in output....
+  
+//"33"= 33
+//"33abc"= NaN
+// true= 1; false= 0
+// null= 0
+ 
+// and similarly we can convert into boolean or in string......
