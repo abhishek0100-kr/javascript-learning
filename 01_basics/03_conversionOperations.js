af357b1f00d1aa1f1ecee0f063298dbcf1e32467
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-20 06:12:08 +0000

    ticky Arithmetic operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,3 +15,25 @@ console.log(valueInNumber);
 // null= 0
  
 // and similarly we can convert into boolean or in string......
+
+
+
+//********* Operations *****************
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3); //modulus
+
+*/
+
+console.log("2" +2 +2 );// 222 would be output 
+console.log(2+2+ "2"); // 42 would  be output
+
+//**** Rule in javascript *****
+// if string is in first then all the literals will be considered as string that's why we got 222 as output
+// if string is in the last then beginning would be normal addition and then string part will come at the end as it is, that's why we got 42 as output (2+2= 4 then "2" => 42)
+
