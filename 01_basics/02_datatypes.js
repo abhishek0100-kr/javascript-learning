24fce09fd1a6b1a72cd2958e5340c387f260178e
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-20 05:51:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // this won't work in nodejs, this only works in browser
+
+//some datatypes
+let name="Abhishek" //string
+let age= 18 //number
+let isLoggedIn= false //boolean
+let state=null //this is not undefined ,this is just empty means value has not been assigned......
+
+
+//numebr => 2 to power 53 range 
+//bigint for bigger values
+//string=""
+// boolean=> true/false
+// null=> standalone value
+// unddefined => 
+// symbol=> unique
+
+//object
+
+console.log(typeof "Abhishek") //typeof tells the type of datatype....
