fbc1637dafe2b748b5974526be6db6f88dda9967
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-23 06:06:04 +0000

    maths and number

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -36,3 +36,24 @@ console.log(Math.ceil(4.6)); //only takes the upper value eg 4.1=5
 console.log(Math.floor(4.6)) //only take lower value eg 4.9=4
 
 
+console.log(Math.min(4,5,9,1,26))
+console.log(Math.max(4,5,9,1,26))
+
+
+//****IMPORTANT ******
+
+
+//DOT random value always comes between o to 1 
+console.log(Math.random())
+
+console.log((Math.random()*10)+1) 
+
+console.log(Math.floor(Math.random()*10)+1) // using floor gives exact value not precised one 
+
+//usually we get value between 0 and 1 . But there is possibility that we might end up getting value like 0.85123456 in thsi case .flor() will give output as 0 . So to avoid this we did +1 .Then we will get value between 1 to 9.
+ 
+
+//In case if we have to define min anx max too then we do like this
+const min=10
+const max= 20
+console.log(Math.floor(Math.random() * (max - min +1)) +min); //Instead of multipying with 10, we did (max-min) and to avoid the case of 0 we add 1. and Addeed min value to get atleast that minimum value
