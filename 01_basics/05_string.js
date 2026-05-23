8423c7e07a098711b38bb9800da1b8251a36dbf2
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-23 04:49:15 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const name="Abhishek"
+const repoCount=30
+
+// console.log(name + repoCount + " value"); // we avoid this way of writing 
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // modern way 
+
+
+const Gname= new String ('Abhishek')
+
+console.log(Gname[0]);
+console.log(Gname.__proto__);
+
+console.log(Gname.length)
+console.log(Gname.toUpperCase());
+
+console.log(Gname.charAt(2)); // element at index 2
+
+console.log(Gname.indexOf('h'))
+
+//simply we can explore more and more about strings 
