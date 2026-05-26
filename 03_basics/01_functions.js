c35eb79570943c0db32b87dbab82fff261ce19ee
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-26 18:23:29 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,33 @@
+const { use } = require("react");
+
+function name(){
+    console.log("A");
+    console.log("B");
+    console.log("H");
+    console.log("I");
+    
+}
+name() //call the function
+
+function sum(num1,num2){
+    console.log(num1+num2);
+    
+}
+sum(2,4) //6 has output
+sum( 2, "3") // 23 output
+sum( 3,null)
+
+
+//another way 
+function sums(num3,num4){
+    let result =num3+num4 
+    return result
+}
+const result=sums(3,5)
+console.log("Result: ",result) 
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Abhishek"));
+//console.log(loginUserMessage);
