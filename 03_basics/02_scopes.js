818ac34ad57ddc5d9b6b6859bde607c140a0f57f
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-26 18:44:42 +0000

    scope ,global scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+
+let a =300
+if(true){
+    let a=10
+    const b=20
+    var c=30 //do not ever use var
+    console.log("INNER: ",a);
+    
+}
+//console.log(a); //error
+//console.log(b); //error
+console.log(c); //but this will give output ,that's why we do not use var
+
+ console.log(a); //takes global scope value ,which is not inside the scope 
