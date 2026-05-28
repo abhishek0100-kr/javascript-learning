ac80dff517606fc0e4fad4b01cad2e3255bc7793
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-28 17:50:30 +0000

    basic if ,else, switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+//if we already know this basics
+
+/* const temp=42
+if(temp<50){
+    console.log("less then 50"); 
+}
+else{
+    console.log("temperature greater than 50");
+
+}
+*/
+
+/*
+switch (key){
+    case value:
+
+        break;
+
+    default:
+
+        break;
+}
+*/
+const month=3 
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+
+    default:
+        console.log("nothing ");
+        break;
+}
+
+
