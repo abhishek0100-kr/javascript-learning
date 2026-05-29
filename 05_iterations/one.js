b828101f48a5b30ea4c90c00190e1dbf161ea35a
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-29 19:26:45 +0000

    for loop, continue,break

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+//for
+/*
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+//just basic things that we know already 
+*/
+
+//break and continue 
+
+for (let index = 1; index < 20; index++) {
+    if(index==5){
+        console.log(`detected 5`);
+        continue
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+    
+}
