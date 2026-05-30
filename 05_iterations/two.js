af8256fd7602ee1b0d6dcf9f2027c7319d9ffe8b
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-05-30 07:34:54 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+
+/*
+while (condition) {
+    
+}
+*/
+
+let index=0
+whilel(index<=10){
+    console.log(`value of index is ${index} `);
+    index=index+2
+    
+}
+
+let array=['flash' ,'batman' , "superman"]
+let arr=0
+while (arr<array.length){
+    console.log(`value is ${array[arr]}`);
+    arr=arr +1
+    
+}
+
+/*
+do {
+    
+} while (condition);
+
+*/
+
+
+do{
+    console.log(`score is ${score} `);
+    score++
+    
+}while(score<=10)
